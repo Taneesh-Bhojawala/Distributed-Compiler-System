ff459af8956c8ad023d0fe1cf931021e700e9b01
Taneesh-Bhojawala <taneeshbhojawala@gmail.com>
2026-04-22 19:10:17 +0530

    Initial Commit and basic include libraries

---
+++ include/common.h
@@ -0,0 +1,17 @@
+#ifndef COMMON_H
+#define COMMON_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <fcntl.h>
+#include <pthread.h>
+#include <sys/types.h>
+#include <string.h>
+
+#define MAX_BUFF 4096
+#define PORT 8080
+
+#endif
