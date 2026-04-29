36e3bb0d679fd89ae60832edf1b2f33739aba5e9
Taneesh-Bhojawala <taneeshbhojawala@gmail.com>
2026-04-29 09:42:45 +0530

    Restructured and fixed workers bug

--- tests/test_conc.sh
+++ tests/test_conc.sh
@@ -1,7 +1,7 @@
 echo "Firing 3 concurrent clients at the Master..."
 
-../src/client test_dir/ taneesh 1234&
-../src/client test_dir2/ taneesh 1234&
+../src/client/client test_dir/ taneesh 1234&
+../src/client/client test_dir2/ taneesh 1234&
 
 wait
 echo "All concurrent transfers complete!"
